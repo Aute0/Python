19b14bc69fa8fa09a9166b4a959f0a3272604418
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-09-29 14:40:50 +0300

    prog calc sum of num in user sequence formula

--- hw2/hw2_ex3.py
+++ hw2/hw2_ex3.py
@@ -1,12 +1,12 @@
 ##  Задайте список из n чисел последовательности $(1 +\frac 1 n) ^ n$ и выведите на экран их сумму.
 #   Пример: - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-UserNumber = int(input('Введите число:  '))
+UserNumber = float(input('Введите число:  '))
 Result = 0
-SumResult = 0
+SumResult = 2
 while UserNumber > 1:
-    Result = (1 +1/(UserNumber))^(UserNumber):
+    Result = (1 +1/(UserNumber))**(UserNumber)
     SumResult = SumResult + Result
     UserNumber -= 1
 
-print(UserNumber + ': ' + SumResult)
+print(UserNumber, SumResult)
