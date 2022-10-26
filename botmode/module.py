f704b6c18066c9b4da46903fcfd76bd752f32a2d
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-10-26 19:36:01 +0300

    add module with basic operation to calculator

---
+++ botmode/module.py
@@ -0,0 +1,20 @@
+x = 0
+y = 0
+
+def init(a, b):
+    global x
+    global y
+    x = a
+    y = b
+
+def mult():
+    return x * y
+
+def minus():
+    return x - y
+
+def summ():
+    return x + y
+
+def devide():
+    return x / y
